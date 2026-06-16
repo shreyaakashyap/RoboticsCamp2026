17cc3b8b4ff1837ff92554329cecb564eb633d42
Shreyaa Kashyap <44851128+shreyaakashyap@users.noreply.github.com>
2026-06-16 14:04:55 -0400

    Add files via upload

---
+++ week2/MicroPy2.7.py
@@ -0,0 +1,12 @@
+import machine
+import utime
+
+slide_switch = machine.Pin(14, machine.Pin.IN)
+
+while True:
+    switch_state = slide_switch.value()
+    if switch_state == 1:
+        print("Switch is toggled to the LEFT!")
+    else:
+        print("Switch is toggled to the RIGHT!")
+    utime.sleep(0.5)
