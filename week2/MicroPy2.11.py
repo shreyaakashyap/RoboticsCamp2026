790e6425cbf67195b5a9b6ca86a080d8bb8f41c7
Shreyaa Kashyap <44851128+shreyaakashyap@users.noreply.github.com>
2026-06-18 13:36:22 -0400

    Add files via upload

---
+++ week2/MicroPy2.11.py
@@ -0,0 +1,15 @@
+import machine
+import utime
+
+potentiometer = machine.ADC(28)
+
+led = machine.PWM(machine.Pin(15))
+led.freq(1000)
+
+while True:
+    value = potentiometer.read_u16()
+    print("Potentiometer value:", value)
+
+    led.duty_u16(value)
+
+    utime.sleep_ms(200)
