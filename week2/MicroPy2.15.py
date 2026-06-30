3514d6fbcd319c1508108ac5bb1c97992aa951c6
Shreyaa Kashyap <44851128+shreyaakashyap@users.noreply.github.com>
2026-06-30 16:12:49 -0400

    Add files via upload

---
+++ week2/MicroPy2.15.py
@@ -0,0 +1,14 @@
+import machine
+led = machine.Pin('LED',machine.Pin.OUT)
+led.value(1)
+
+# Initialize the button and signal pins
+button = machine.Pin(14, machine.Pin.IN)
+signal = machine.Pin(15, machine.Pin.OUT)
+
+while True:
+    button_status = button.value()
+    if button_status == 1:
+        signal.value(1)  # Send high signal to the transistor
+    else:
+        signal.value(0)  # Send low signal to the transistor
